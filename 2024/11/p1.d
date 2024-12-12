ccb5b8bb26559ab683066ca068f31e7821bbeebc
Robin Moalic <robin.moalic@yahoo.fr>
2024-12-12 15:46:04 +0100

    2024: 11

--- 2024/11/p1.d
+++ 2024/11/p1.d
@@ -1,8 +1,8 @@
-import std.stdio, std.file, std.conv, std.algorithm, std.string, std.range, std.container, std.math, std.functional;
+import std.stdio, std.file, std.conv, std.algorithm, std.string, std.range, std.container, std.math, std.functional, std.typecons, std.bigint;
 
 string input_file = "input.txt";
 
-uint nb_digits(long n) {
+uint nb_digits(BigInt n) {
   if (n == 0) return 0;
   if (n < 0) n = -n;
   uint ret = 0;
@@ -17,11 +17,11 @@ bool is_odd(long n) {
   return n & 1;
 }
 
-long[] blink(ref long[] input) {
-  long[] ret;
+BigInt[] blink(BigInt[] input) {
+  BigInt[] ret;
   foreach (d; input) {
     if (d == 0) {
-      ret ~= 1;
+      ret ~= d + 1;
     } else {
       auto digits = nb_digits(d);
       if (! is_odd(digits)) {
@@ -39,80 +39,60 @@ long[] blink(ref long[] input) {
 }
 
 ulong part1(in Dinput content) {
-  auto data = cast(long[]) content.input.dup;
+  auto data = cast(BigInt[]) content.input.dup;
   foreach (i; iota(25)) {
-    //writeln("After ", i + 1, " blink");
     data = blink(data);
-    //writeln(data.length);
-    //writeln(data);
   }
   return data.length;
 }
 
-ulong count_progress(long input, uint steps) {
-  if (steps == 0) return 1;
-  if (input == 0) return count_progress(1, steps - 1);
+alias progress_memoize = memoize!(progress);
+
+BigInt[] progress(BigInt input) {
+  if (input == 0) return [input + 1];
   auto digits = nb_digits(input);
 
   if (! is_odd(digits)) {
-    auto mid = pow(10, digits / 2);
-    auto p1 = input / mid;
-    auto p2 = input - (p1 * mid);
-    return count_progress(p1, steps - 1) + count_progress(p2, steps - 1);
-  } else {
-    return count_progress(input * 2024, steps - 1);
-  }
-}
-
-alias progress_memoize = memoize!(progress, 9604);
-
-long[] progress(long input) {
-  if (input == 0) return [1];
-  auto digits = nb_digits(input);
-
-  if (! is_odd(digits)) {
-    auto mid = pow(10, digits / 2);
-    auto p1 = input / mid;
-    auto p2 = input - (p1 * mid);
+    BigInt mid = pow(10, digits / 2);
+    BigInt p1 = input / mid;
+    BigInt p2 = input - (p1 * mid);
     return [p1, p2];
   } else {
     return [input * 2024];
   }
 }
 
-alias progress2_memoize = memoize!progess2;
+ulong[BigInt] progress2(ulong[BigInt]  step) {
+  ulong[BigInt] ret;
+  foreach (pair; step.byKeyValue()) {
+    auto p = progress_memoize(pair.key);
+    foreach (v; p) {
+      ret[v] += pair.value;
+    }
+  }
 
-long[] progess2(long[] step) {
-  long[] ret;
-  foreach (v; step) {
-    ret ~= progress_memoize(v);
+  return ret;
+}
+
+BigInt process(BigInt[] input, uint steps) {
+  ulong[BigInt] counter = cast(ulong[BigInt])input.sort.group.assocArray;
+  foreach (i; iota(steps)) {
+    counter = progress2(counter);
+    //assert (! counter.byKey.map!(c => c.ulongLength).array.any!(c => c > 1));
+  }
+  BigInt ret = 0;
+  foreach (b; counter.byValue) {
+    ret += b;
   }
   return ret;
 }
 
-ulong process(long input, uint steps) {
-  long[] step = [input];
-  auto count = 0;
-  foreach (i; iota(steps)) {
-    step = progress2_memoize(step);
-    count++;
-    writeln("finished step: ", count);
-  }
-  return step.length;
+BigInt part2(in Dinput content) {
+  return process(content.input.dup, 75);
 }
 
-ulong part2(in Dinput content) {
-  auto data = cast(long[]) content.input.dup;
-  ulong ret = 0;
-  foreach (nb, d; content.input) {
-    ret += process(d, 75);
-    writeln("total progess: ", nb / (content.input.length - 1 * 1.0) * 100.0);
-  }
-
-  return ret;}
-
 struct Dinput {
-  long[] input;
+  BigInt[] input;
 }
 
 void main() {
@@ -122,10 +102,9 @@ void main() {
   while (! input.eof()) {
     string line = input.readln().strip;
     if (line.count() != 0) {
-      din.input ~= line.split(" ").map!(to!long).array;
+      din.input ~= line.split(" ").map!(to!BigInt).array;
     }
   }
-  writeln(din);
 
   auto p1 = part1(din);
   auto p2 = part2(din);
